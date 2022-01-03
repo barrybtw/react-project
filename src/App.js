27d2e6a5f1b3cc8fee44c8772a95890e7bfd4f54
barry <nicolai.bernhard03@gmail.com>
2022-01-03 21:55:15 +0100

    Pages added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { AuthContext } from "./context/AuthProvider";
 
 //Pages import, 1 line thanks.
-import { Home } from "./pages";
+import { Login, Conversation, Profile } from "./pages";
 
 export const App = () => {
   const [isAuthenticated, setisAuthenticated] = useState(false);
@@ -14,7 +14,9 @@ export const App = () => {
     <Router>
       <AuthContext.Provider value={(isAuthenticated, setisAuthenticated)}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/conversation/:id" element={<Login />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </AuthContext.Provider>
     </Router>

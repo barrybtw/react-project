5911d590fca5a94b1a08f70a617378411d9a0cd6
barry <nicolai.bernhard03@gmail.com>
2022-01-03 21:23:43 +0100

    Context api added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+//Pages import, 1 line thanks.
+import { Home } from "./pages";
 
 export const App = () => {
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+  //States go here
   return (
     <Router>
-      <Routes>{/* <Route path="/" element={<Thing/>}/> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       buhuhuehefuefbewjfbwfnkfsbckdbvksdnvkdn
     </Router>
   );
